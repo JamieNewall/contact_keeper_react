ef0c85399e97ae8f42740ef354dfa5c702ba8f9d
Jamie Newall <jnewall93@outlook.com>
2020-01-19 19:08:54 +0000

    express added

---
+++ routes/contacts.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+
+// @route GET api/contacts
+// @desc Get all user contacts
+// @access Private
+
+router.get('/',(req,res) => {
+  res.send('get all contacts');
+});
+
+// @route POST api/contacts
+// @desc Add new contact
+// @access Private
+
+router.post('/',(req,res) => {
+  res.send('add contact');
+});
+
+// @route PUT api/contacts/:id
+// @desc Update contacts
+// @access Private
+
+router.put('/:id',(req,res) => {
+  res.send('update contact');
+});
+
+// @route DELETE api/contacts/:id
+// @desc Delete contact
+// @access Private
+
+router.delete('/:id',(req,res) => {
+  res.send('delete contact');
+});
+
+
+module.exports = router; 
